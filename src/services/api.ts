02c9c819dd347e469aa06b3e5c7ab22921f5856f
joiceclemente <joice.clemente@hotmail.com>
2025-04-02 22:55:14 -0300

    baseURL atualizada

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://02e5-168-90-7-86.ngrok-free.app',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
 // Função para obter tradução
